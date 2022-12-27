6ac1947bc18d19472b8135117f5fb9902dd901a5
Jason Evangelista <jasonev.dev@gmail.com>
2022-12-27 05:54:58 +0000

    feat: add routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import React, { lazy } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 // import logo from './logo.svg';
 import './App.css';
@@ -6,12 +11,19 @@ import './App.css';
 // import Loading from './components/Loading';
 
 const Login = lazy(() => import('./pages/Login'));
+const Home = lazy(() => import('./pages/Home'));
 
 function App() {
   return (
-
-    <Login />
-
+    <div>
+      {/* <Login /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
